59fafa02943e5264f9f009925658207b61c91f58
Brian <brian@tailboardtech.com>
2018-04-12 12:28:51 -0400

    completed lesson 5, added button controls to show other view controllers

--- swoosh/ViewController.swift
+++ swoosh/ViewController.swift
@@ -18,11 +18,7 @@ class ViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+            
     }
-
-
 }
-
